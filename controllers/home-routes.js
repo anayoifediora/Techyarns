095943a7fb73049e9906060171912a78fd3e8d22
Kanayo <anayoifediora@live.com>
2023-07-09 00:00:17 +0930

    started creation of handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -15,7 +15,7 @@ router.get('/', async (req, res) => {
         // res.status(200).json(dbPostData)
         const homePosts = dbPostData.map((homePost) => 
         homePost.get({ plain: true }));
-
+        console.log(homePosts)
         res.render('homepage', {
             homePosts, loggedIn: req.session.loggedIn,
         });
