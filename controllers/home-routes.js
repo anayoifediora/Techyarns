3af850abbd1047904fe153a899d00452460e8b13
Kanayo <anayoifediora@live.com>
2023-07-11 13:57:56 +0930

    enabled home, dashboard and logout links when logged in

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -32,9 +32,9 @@ router.get('/login', (req, res) => {
     }
   
     res.render('login');
-  });
+});
   
-  router.get('/posts/:id', async (req, res) => {
+router.get('/posts/:id', async (req, res) => {
     try {
         const postData = await Post.findByPk(req.params.id, {
             include: [
