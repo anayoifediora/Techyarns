233ec4be4723e53dbc541fa914287466ff3bdba5
Kanayo <anayoifediora@live.com>
2023-09-11 00:33:01 +0930

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,12 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // Creates a connection to the database
-const sequelize = new Sequelize(
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+ sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
@@ -11,7 +16,8 @@ const sequelize = new Sequelize(
       host: 'localhost',
       dialect: 'mysql',
       port: 3306,
-    }
+    },
   );
+}
   
   module.exports = sequelize;
